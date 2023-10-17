a163c9df6277d0ddd1950d297ea5a4ee6bf1b13e
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-10-17 19:38:41 -0300

    footer and navbar components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <span className="footer-info">Todos os direitos de Jujutsu Kaisen são reservados a Gege Akutami</span>
+            <span className="footer-info">Kaisen Backup</span>
+        </footer>
+    )
+}
+
+export default Footer;
