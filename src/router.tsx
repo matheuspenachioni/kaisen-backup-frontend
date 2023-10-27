1ded0b82bbc3adbc8f9f55d285d698f7bda9965d
Juan Almeida <juanalgal2@gmail.com>
2023-10-27 09:29:21 -0300

    feature(user-reading-history): naming convention

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import Navbar from '@components/Navbar/Navbar'
-import React, { Suspense } from 'react'
+import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import {
   Anime,
